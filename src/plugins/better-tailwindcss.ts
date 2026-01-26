71373cdcfc4116c028ef738bd77681061338bb47
Achmad F. Ibrahim <acfatah@gmail.com>
2026-01-26 08:30:48 +0800

    fix: unable to build source
    The error message: src/plugins/better-tailwindcss.ts(4,61):
    error TS2694: Namespace '"/home/acfatah/2026/eslint-preset/node_modules
    /eslint/lib/types/index".Linter' has no exported member 'Plugin'

--- src/plugins/better-tailwindcss.ts
+++ src/plugins/better-tailwindcss.ts
@@ -1,6 +1,8 @@
+import type { ESLint } from 'eslint'
+
 import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
-export const betterTailwindcssPlugin = {
+export const betterTailwindcssPlugin: Record<string, ESLint.Plugin> = {
   // https://github.com/schoero/eslint-plugin-better-tailwindcss
   'better-tailwindcss': eslintPluginBetterTailwindcss,
 }
